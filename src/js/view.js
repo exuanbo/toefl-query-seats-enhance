639cacfaeaf5174ceccaecfd123188074575e337
exuanbo <exuanbo@protonmail.com>
2020-08-25 09:37:33 +0800

    refactor: helper functions

--- src/js/view.js
+++ src/js/view.js
@@ -46,8 +46,17 @@ const clearResult = () => {
 const isMunicipality = cityName => cityName === '北京' || cityName === '上海' || cityName === '天津' || cityName === '重庆'
 
 const renderTpl = filteredData => {
-  const helper = { formatCurrency: value => 'RMB￥' + value.toFixed(2) }
-  const firstKeyOf = obj => Object.keys(obj)[0]
+  const helper = {
+    formatCurrency: value => 'RMB￥' + value.toFixed(2),
+    firstKeyOf: obj => Object.keys(obj)[0],
+    isEmpty: el => {
+      for (const childNode of el.childNodes) {
+        if (childNode.nodeValue.replace(/\s*/g, '') === '' && childNode.nodeName !== '#comment') return false
+      }
+      return true
+    }
+  }
+
   const rowTpl = seat => html`
     <tr>
       <td style="text-align:center;vertical-align:middle;">
@@ -79,15 +88,8 @@ const renderTpl = filteredData => {
     </tr>
   `
 
-  const isEmpty = el => {
-    for (const childNode of el.childNodes) {
-      if (childNode.nodeValue.replace(/\s*/g, '') === '' && childNode.nodeName !== '#comment') return false
-    }
-    return true
-  }
-
   const seatsTpl = data => html`
-    ${isEmpty(document.getElementById('qrySeatResult'))
+    ${helper.isEmpty(document.getElementById('qrySeatResult'))
       ? html`<h4>考位查询结果</h4>
           <div>"<span style="color:red;">*</span>"表示为逾期报名，需要缴纳逾期报名附加费${helper.formatCurrency(data.lateRegFee/100)}</div>
         `
@@ -96,7 +98,7 @@ const renderTpl = filteredData => {
     <table class="table table-bordered" style="margin-top:12px;font-size:16px;">
       <thead>
         <tr style="background-color:#993333;">
-          <th colspan="4"><span style="color:#fff;">考试日期：${data.testDate}</span><span style="margin-left:.5em;color:#fff;"><i class="fa fa-calendar-check-o" aria-hidden="true"></i></span><span style="color:#fff;float:right;">考试时间：${firstKeyOf(data.testSeats).split('|')[0]}<span style="padding-left:30px;">最晚到达时间：${firstKeyOf(data.testSeats).split('|')[2]}</span></span></th>
+          <th colspan="4"><span style="color:#fff;">考试日期：${data.testDate}</span><span style="margin-left:.5em;color:#fff;"><i class="fa fa-calendar-check-o" aria-hidden="true"></i></span><span style="color:#fff;float:right;">考试时间：${helper.firstKeyOf(data.testSeats).split('|')[0]}<span style="padding-left:30px;">最晚到达时间：${helper.firstKeyOf(data.testSeats).split('|')[2]}</span></span></th>
         </tr>
         <tr>
           <th style="text-align:center;vertical-align:middle;" width="20%">城市</th>
@@ -106,7 +108,7 @@ const renderTpl = filteredData => {
         </tr>
       </thead>
       <tbody>
-        ${data.testSeats[firstKeyOf(data.testSeats)].map(seat => html`${rowTpl(seat)}`)}
+        ${data.testSeats[helper.firstKeyOf(data.testSeats)].map(seat => html`${rowTpl(seat)}`)}
       </tbody>
     </table>
   `

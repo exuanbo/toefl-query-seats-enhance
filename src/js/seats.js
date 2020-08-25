41eaf587a53d2032811c9f9df22a7eda7bf1c32c
exuanbo <exuanbo@protonmail.com>
2020-08-25 22:12:33 +0800

    refactor: add comments for data structure

--- src/js/seats.js
+++ src/js/seats.js
@@ -1,3 +1,33 @@
+/**
+ * @example
+ * {
+    "status":true,
+    "testDate":"2020年12月20日 星期日",
+    "testSeats":{
+     "09:00|20201220|08:30":[
+       {
+         "provinceCn":"甘肃",
+         "provinceEn":"GANSU",
+         "cityCn":"兰州",
+         "cityEn":"LANZHOU",
+         "centerCode":"STN80013A",
+         "centerNameCn":"兰州大学",
+         "centerNameEn":"LANZHOU UNIVERSITY",
+         "testFee":210000,
+         "lateReg":"N",
+         "seatStatus":1,
+         "seatBookStatus":0,
+         "rescheduleDeadline":1608134399000,
+         "cancelDeadline":1608134399000,
+         "testTime":"09:00",
+         "lateRegFlag":"N"
+        }
+      ]
+    },
+    "lateRegFee":31000
+  }
+ */
+
 const filterSeats = data => {
   if (data.status === true) {
     const dataDate = Object.keys(data.testSeats)[0]

79f68eb88a4e281cfc23805ec57fe98e902ee2e2
exuanbo <exuanbo@protonmail.com>
2020-09-09 22:26:13 +0800

    chore: add userscript meta data

---
+++ src/extension/meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         托福考位查询增强
+// @namespace    https://github.com/exuanbo
+// @version      2.0.0
+// @description  一键查询所选地点所有时间的可预定考位
+// @author       Exuanbo
+// @match        https://toefl.neea.cn/myHome/*
+// @icon         https://raw.githubusercontent.com/exuanbo/toefl-query-seats-enhance/master/src/img/icon48.png
+// ==/UserScript==

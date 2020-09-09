3ce0deb72cb7b870f30b0c9cd27eb88b7a1fb16c
exuanbo <exuanbo@protonmail.com>
2020-09-09 23:16:25 +0800

    chore(meta.js): add `@updateURL`, `@downloadURL`

--- src/extension/meta.js
+++ src/extension/meta.js
@@ -5,6 +5,8 @@
 // @author       Exuanbo
 // @description  一键查询所选地点所有时间的可预定考位
 // @icon         https://raw.githubusercontent.com/exuanbo/toefl-query-seats-enhance/master/src/img/icon48.png
+// @updateURL    https://gist.github.com/exuanbo/32ebda63925b60e7817ba0346b980d14/raw/toefl-query-seats-enhance.user.js
+// @downloadURL  https://gist.github.com/exuanbo/32ebda63925b60e7817ba0346b980d14/raw/toefl-query-seats-enhance.user.js
 // @match        https://toefl.neea.cn/myHome/*
 // @grant        none
 // ==/UserScript==

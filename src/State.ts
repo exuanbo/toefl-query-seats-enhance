2a2a6a81127da5a52d11e06c83a9fdde675f7207
exuanbo <exuanbo@protonmail.com>
2020-10-25 11:32:29 +0000

    refactor: extract class `StateData` fron `State`

--- src/State.ts
+++ src/State.ts
@@ -2,56 +2,59 @@ import { calcLeft } from './utils'
 import * as render from './views/render'
 import { selectedCity, availableDates } from './views/get'
 
-interface StateData {
+class StateData {
   city?: string
   cities?: string[]
-  currentCity?: string
-  citiesLeft?: number
+  currentCity: string
+  citiesLeft: number
 
-  dates?: string[]
-  currentDate?: string
-  datesLeft?: number
+  dates = availableDates()
+  currentDate: string
+  datesLeft: number
 
-  sum?: number
-  progress?: number
+  sum: number
+  progress = 0
 
-  availableSeats?: number
-  err?: number
-  isComplete?: boolean
-}
-
-export class State {
-  private data: StateData = {
-    dates: dates(),
-    availableSeats: 0,
-    err: 0
-  }
-
-  get = <K extends keyof StateData>(key: K): StateData[K] | undefined => {
-    return this.data[key]
-  }
-
-  set = (newData: StateData, update = false): void => {
-    Object.assign(this.data, newData)
-    if (update) this.update()
-  }
+  availableSeats = 0
+  err = 0
+  isComplete = false
 
   constructor () {
     const city = selectedCity()
     if (city instanceof Array && city.length !== 1) {
-      this.data.cities = city
+      this.cities = city
     } else if (city === '-1') {
       return
     } else {
       const singleCity = city instanceof Array ? city[0] : city
-      this.data.city = singleCity
+      this.city = singleCity
     }
-    this.data.sum = this.data.dates.length * (this.data.city !== undefined ? 1 : this.data.cities.length)
+    this.sum = this.dates.length * (this.city !== undefined ? 1 : this.cities.length)
+  }
+}
+
+export class State {
+  private data = new StateData
+
+  constructor () {
     this.update()
   }
 
+  get <K extends keyof StateData>(key: K): StateData[K] | undefined {
+    return this.data[key]
+  }
+
+  set (newData: Partial<StateData>, update = false): void {
+    Object.assign(this.data, newData)
+    if (update) {
+      this.update()
+    }
+  }
+
   private update (): void {
-    if (this.data.cities !== undefined) this.data.citiesLeft = calcLeft(this.data.currentCity, this.data.cities)
+    if (this.data.cities !== undefined) {
+      this.data.citiesLeft = calcLeft(this.data.currentCity, this.data.cities)
+    }
     this.data.datesLeft = calcLeft(this.data.currentDate, this.data.dates)
     this.data.progress =
       100 -

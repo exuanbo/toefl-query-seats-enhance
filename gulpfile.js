bc76831cb2dea3f30b6e33d324beb3294bb340f5
exuanbo <exuanbo@protonmail.com>
2020-11-30 21:35:17 +0000

    build: refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,76 @@
 import fs from 'fs'
-import path from 'path'
 import { exec } from 'child_process'
+import path from 'path'
+
 import gulp from 'gulp'
 import through from 'through2'
 import source from 'vinyl-source-stream'
 import terser from 'gulp-terser'
+
 import rollupStream from '@rollup/stream'
-import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
 import minifyHtml from 'rollup-plugin-minify-html-template-literals'
 
 const { src, dest, series, parallel, watch } = gulp
 
-const header = filePath => {
-  return through.obj((file, _, callback) => {
-    const headerContent = String(fs.readFileSync(path.join(process.cwd(), filePath)))
+const header = filePath =>
+  through.obj((file, _, callback) => {
+    const headerContent = String(
+      fs.readFileSync(path.join(process.cwd(), filePath))
+    )
     file.contents = Buffer.from(headerContent + String(file.contents))
     callback(null, file)
   })
-}
 
-function clean () {
-  return exec('rm -rf dist')
-}
+const clean = () => exec('rm -rf dist')
 
-function build () {
+const build = () => {
   const options = {
     input: 'src/index.ts',
     output: { format: 'iife' },
-    plugins: [nodeResolve({ browser: true }), commonjs(), minifyHtml(), typescript()]
+    plugins: [
+      nodeResolve({ browser: true }),
+      commonjs(),
+      minifyHtml(),
+      typescript()
+    ]
   }
   return rollupStream(options)
     .pipe(source('app.js'))
     .pipe(dest('dist/extension'))
 }
 
-function addHeader () {
-  return src('dist/extension/app.js')
+const addHeader = () =>
+  src('dist/extension/app.js')
     .pipe(header('src/extension/meta.js'))
     .pipe(dest('dist/userscript'))
-}
 
-function minifyJS () {
-  return src('dist/extension/app.js', { base: '.' })
-    .pipe(terser())
-    .pipe(dest('.'))
-}
+const minifyJS = () =>
+  src('dist/extension/app.js', { base: '.' }).pipe(terser()).pipe(dest('.'))
 
-function mix () {
-  return src(['src/extension/content.js', 'src/extension/manifest.json', 'src/img/*']).pipe(
-    dest('dist/extension')
+const mix = () =>
+  src([
+    'src/extension/content.js',
+    'src/extension/manifest.json',
+    'src/img/*'
+  ]).pipe(dest('dist/extension'))
+
+const pack = () => {
+  const name = 'extension'
+  return exec(
+    `cd dist/${name} && zip -r ${name}.zip . && mv ${name}.zip ../${name}.zip`
   )
 }
 
-function pack () {
-  const name = 'extension'
-  return exec(`cd dist/${name} && zip -r ${name}.zip . && mv ${name}.zip ../${name}.zip`)
-}
-
-function server () {
+const server = () => {
   watch('src/**/*', { ignoreInitial: false }, build)
 }
 
-export default series(clean, parallel(series(build, parallel(addHeader, minifyJS)), mix), pack)
+export default series(
+  clean,
+  parallel(series(build, parallel(addHeader, minifyJS)), mix),
+  pack
+)
 export { server }
